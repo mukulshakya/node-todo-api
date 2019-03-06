7b4c876778035508535c6028a0e7522e2698f8f3
Mukul Shakya <muklshakya123@gmail.com>
2019-03-06 17:30:09 +0530

    Heroku Setup

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/NodeTodoApp',{ useNewUrlParser: true });
+mongoose.connect('mongodb://heroku_x3h7btp3:t8lcq6ptfgn2v2qdahflssnndp@ds161285.mlab.com:61285/heroku_x3h7btp3' || 'mongodb://localhost:27017/NodeTodoApp', { useNewUrlParser: true });
 
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const {ObjectID} = require('mongodb');
 const express = require('express');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -12,6 +13,10 @@ const port = process.env.PORT || 3000
 // const port = process.env.OPENSHIFT_NODEJS_PORT || 3000
 
 app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('App Working Fine\n\nOther Routes Available\n* GET /todos\n* GET /todos/:id\n* POST /todos\n* DELETE /todos/:id');
+})
 
 app.post('/todos', (req, res) => {
     var todo = new Todo({
@@ -59,7 +64,28 @@ app.delete('/todos/:id', (req, res) => {
 
         res.send({todos});
     }).catch((err) => res.status(400).send());
-})
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', "completed"]);
+
+    if(!ObjectID.isValid(id)) return res.status(404).send();
+
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    }
+    else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findOneAndUpdate({_id: id}, {$set: body}, {new: true}).then((todo) => {
+        if(!todo) return res.status(404).send();
+
+        res.send({todo});
+    }).catch(err => res.status(400).send());
+});
 
 app.listen(port, () => console.log(`Starting on Port ${port}`));
 
